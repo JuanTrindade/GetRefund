e17a1cddb3ead9ff8d6e91ae25a9259bbeaacb6d
Pedro Ramos <pedroramos.contato@outlook.com>
2020-04-05 01:36:29 -0300

    :art: Indentar componentes

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import 'react-native-gesture-handler';
-import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Main from './index';
@@ -16,17 +15,31 @@ const Stack = createStackNavigator();
 
 export default function Routes(){
     return(
-        <NavigationContainer>
-            <Stack.Navigator headerMode='none'>
-                <Stack.Screen name="index" component={Main}/>
-                <Stack.Screen name="cad" component={cadastrar}/>
-                <Stack.Screen name="login" component={login}/>
-                <Stack.Screen name="loginAdm" component={loginAdm}/>
-                <Stack.Screen name="CadUser" component={CadUser}/>
-                <Stack.Screen name="HomeAdm" component={HomeAdm}/>
-                <Stack.Screen name="HomeUser" component={HomeUser}/>
-                <Stack.Screen name="cameraPage" component={cameraPage}/>
-            </Stack.Navigator>
-        </NavigationContainer>
+        <Stack.Navigator headerMode='none'>
+            <Stack.Screen 
+                name="index" 
+                component={Main}/>
+            <Stack.Screen 
+                name="cad" 
+                component={cadastrar}/>
+            <Stack.Screen 
+                name="login" 
+                component={login}/>
+            <Stack.Screen 
+                name="loginAdm" 
+                component={loginAdm}/>
+            <Stack.Screen 
+                name="CadUser" 
+                component={CadUser}/>
+            <Stack.Screen 
+                name="HomeAdm" 
+                component={HomeAdm}/>
+            <Stack.Screen 
+                name="HomeUser" 
+                component={HomeUser}/>
+            <Stack.Screen 
+                name="cameraPage" 
+                component={cameraPage}/>
+        </Stack.Navigator>
     );
 }
